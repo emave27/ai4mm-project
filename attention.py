44e51db3d72299ddfe9c18e57d60e583a1f9bf45
emave27 <48360360+emave27@users.noreply.github.com>
2022-03-10 11:38:24 +0100

    Update attention.py

--- attention.py
+++ attention.py
@@ -1,8 +1,6 @@
 import tensorflow as tf
 from keras.layers import Dense, Layer
 
-#tf.keras.layers.Layer
-
 class BahdanauAttention(Layer):
   def __init__(self, units, verbose=0):
     super(BahdanauAttention, self).__init__()
@@ -12,23 +10,23 @@ class BahdanauAttention(Layer):
     self.verbose= verbose
 
   def call(self, query, values):
+    #values shape: (batch_size, max_len, hidden size) --> encoder output shape
+    #query shape: (batch_size, hidden size) --> decoder output shape
 
-    #query hidden state shape == (batch_size, hidden size)
-    #query_with_time_axis shape == (batch_size, 1, hidden size)
-    #values shape == (batch_size, max_len, hidden size)
-    #broadcast addition along the time axis to calculate the score
-    query_with_time_axis = tf.expand_dims(query, 1)
-  
-
-    #score shape == (batch_size, max_length, 1)
-    #we get 1 at the last axis because we are applying score to self.V
-    #the shape of the tensor before applying self.V is (batch_size, max_length, units)
-    score = self.V(tf.nn.tanh(self.W1(query_with_time_axis) + self.W2(values)))
-
-    attention_weights = tf.nn.softmax(score, axis=1)
-
-    #context_vector shape after sum == (batch_size, hidden_size)
-    context_vector = attention_weights * values
-    context_vector = tf.reduce_sum(context_vector, axis=1)
+    #reshape query tensor to compute score value
+    query_with_time_axis=tf.expand_dims(query, 1)
+    #query_with_time_axis shape: (batch_size, 1, hidden size)
+    
+    #compute alignment score
+    score=self.V(tf.nn.tanh(self.W1(query_with_time_axis) + self.W2(values)))
+    #score shape: (batch_size, max_length, 1)
+    
+    #apply softmax to score to get attention weights
+    attention_weights=tf.nn.softmax(score, axis=1)
+    
+    #compute the context vector
+    context_vector=attention_weights * values
+    context_vector=tf.reduce_sum(context_vector, axis=1)
+    #context_vector shape after sum: (batch_size, hidden_size)
 
     return context_vector, attention_weights
